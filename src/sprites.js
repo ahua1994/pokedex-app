41a9ea67d63e780eb2a17a6ef041c7030d6dd711
Andy Hua <user.andy.hua@gmail.com>
2022-12-07 17:06:32 -0800

    fixed 403 icons

--- src/sprites.js
+++ src/sprites.js
@@ -1,22 +1,41 @@
+import bug from "./sprites/BugIC_BDSP.png";
+import dark from "./sprites/DarkIC_BDSP.png";
+import dragon from "./sprites/DragonIC_BDSP.png";
+import electric from "./sprites/ElectricIC_BDSP.png";
+import fairy from "./sprites/FairyIC_BDSP.png";
+import fighting from "./sprites/FightingIC_BDSP.png";
+import fire from "./sprites/FireIC_BDSP.png";
+import flying from "./sprites/FlyingIC_BDSP.png";
+import ghost from "./sprites/GhostIC_BDSP.png";
+import grass from "./sprites/GrassIC_BDSP.png";
+import ground from "./sprites/GroundIC_BDSP.png";
+import ice from "./sprites/IceIC_BDSP.png";
+import normal from "./sprites/NormalIC_BDSP.png";
+import poison from "./sprites/PoisonIC_BDSP.png";
+import psychic from "./sprites/PsychicIC_BDSP.png";
+import rock from "./sprites/RockIC_BDSP.png";
+import steel from "./sprites/SteelIC_BDSP.png";
+import water from "./sprites/WaterIC_BDSP.png";
+
 export const sprites = {
-    bug: "https://archives.bulbagarden.net/media/upload/9/9c/BugIC_BDSP.png",
-    dark: "https://archives.bulbagarden.net/media/upload/b/b9/DarkIC_BDSP.png",
-    dragon: "https://archives.bulbagarden.net/media/upload/c/c7/DragonIC_BDSP.png",
-    electric: "https://archives.bulbagarden.net/media/upload/c/c1/ElectricIC_BDSP.png",
-    fairy: "https://archives.bulbagarden.net/media/upload/d/d6/FairyIC_BDSP.png",
-    fighting: "https://archives.bulbagarden.net/media/upload/3/35/FightingIC_BDSP.png",
-    fire: "https://archives.bulbagarden.net/media/upload/b/b1/FireIC_BDSP.png",
-    flying: "https://archives.bulbagarden.net/media/upload/0/03/FlyingIC_BDSP.png",
-    ghost: "https://archives.bulbagarden.net/media/upload/4/45/GhostIC_BDSP.png",
-    grass: "https://archives.bulbagarden.net/media/upload/a/ad/GrassIC_BDSP.png",
-    ground: "https://archives.bulbagarden.net/media/upload/b/bd/GroundIC_BDSP.png",
-    ice: "https://archives.bulbagarden.net/media/upload/1/11/IceIC_BDSP.png",
-    normal: "https://archives.bulbagarden.net/media/upload/8/89/NormalIC_BDSP.png",
-    poison: "https://archives.bulbagarden.net/media/upload/8/8b/PoisonIC_BDSP.png",
-    psychic: "https://archives.bulbagarden.net/media/upload/6/68/PsychicIC_BDSP.png",
-    rock: "https://archives.bulbagarden.net/media/upload/9/98/RockIC_BDSP.png",
-    steel: "https://archives.bulbagarden.net/media/upload/5/5f/SteelIC_BDSP.png",
-    water: "https://archives.bulbagarden.net/media/upload/2/2b/WaterIC_BDSP.png",
+    bug,
+    dark,
+    dragon,
+    electric,
+    fairy,
+    fighting,
+    fire,
+    flying,
+    ghost,
+    grass,
+    ground,
+    ice,
+    normal,
+    poison,
+    psychic,
+    rock,
+    steel,
+    water,
 };
 export const types = [
     "any",
